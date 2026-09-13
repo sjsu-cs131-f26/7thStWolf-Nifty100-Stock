add9a1007d35da78cc1f6a852689ee12617116bf
Vakeesan Moorthy <Vakeesan.moorthy@gmail.com>
2026-09-13 15:53:12 -0700

    updated all cmds

--- data/cmd.sh
+++ data/cmd.sh
@@ -1,4 +1,5 @@
 git clone git@github.com:sjsu-cs131-f26/Team8_Wolf_of_the_7th_St..git
-#Add Data Ingesition Command
-python3 data/processing.py raw_data processed data
-# Cmd to merge all csv into one
+pip install -r Team8_Market_Predictor/data/requirements.txt
+python 7thStWolf-Nifty100-Stock/data/ingestion.py raw_data
+python 7thStWolf-Nifty100-Stock/data/processing.py raw_data processed_data
+awk 'FNR==1 && NR!=1{next;}{print}' processed_data/*.csv > merged.csv 
