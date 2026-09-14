573cc0d7db31762300b7435f4232f18c8475c65c
SAMVM1128 <vakeesan.moorthy@gmail.com>
2026-09-14 11:13:03 -0700

    updated repo name used

--- data/cmd.sh
+++ data/cmd.sh
@@ -1,5 +1,5 @@
 git clone git@github.com:sjsu-cs131-f26/Team8_Wolf_of_the_7th_St..git
-pip install -r Team8_Market_Predictor/data/requirements.txt
+pip install -r 7thStWolf-Nifty100-Stock/data/requirements.txt
 python 7thStWolf-Nifty100-Stock/data/ingestion.py raw_data
 python 7thStWolf-Nifty100-Stock/data/processing.py raw_data processed_data
 awk 'FNR==1 && NR!=1{next;}{print}' processed_data/*.csv > merged.csv 
