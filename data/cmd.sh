053f7801717c66cac88f90fc88eced55d5316ae4
SAMVM1128 <vakeesan.moorthy@gmail.com>
2026-09-14 11:16:47 -0700

    updated git clone

--- data/cmd.sh
+++ data/cmd.sh
@@ -1,4 +1,4 @@
-git clone git@github.com:sjsu-cs131-f26/Team8_Wolf_of_the_7th_St..git
+git clone git@github.com:sjsu-cs131-f26/7thStWolf-Nifty100-Stock.git
 pip install -r 7thStWolf-Nifty100-Stock/data/requirements.txt
 python 7thStWolf-Nifty100-Stock/data/ingestion.py raw_data
 python 7thStWolf-Nifty100-Stock/data/processing.py raw_data processed_data
