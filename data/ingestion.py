0590cbb490ce61518756f199ce3d673ca91a9808
SAMVM1128 <vakeesan.moorthy@gmail.com>
2026-09-14 11:13:26 -0700

    fixed data path

--- data/ingestion.py
+++ data/ingestion.py
@@ -6,5 +6,4 @@ folder_name = sys.argv[1]
 raw_data_dir = os.path.join(os.getcwd(), folder_name)
 os.makedirs(raw_data_dir, exist_ok=True)
 
-path = kagglehub.dataset_download("oddrationale/mnist-in-csv", output_dir=raw_data_dir)
-#debashis74017/stock-market-data-nifty-50-stocks-1-min-data
+path = kagglehub.dataset_download("debashis74017/stock-market-data-nifty-50-stocks-1-min-data", output_dir=raw_data_dir)
