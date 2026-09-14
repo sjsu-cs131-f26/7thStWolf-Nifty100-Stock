51320ff9c29aa14176fd7d968504792defef6f40
SAMVM1128 <vakeesan.moorthy@gmail.com>
2026-09-14 12:53:59 -0700

    added correct dataset path

--- data/ingestion.py
+++ data/ingestion.py
@@ -10,6 +10,6 @@ except:
 raw_data_dir = os.path.join(os.getcwd(), folder_name)
 os.makedirs(raw_data_dir, exist_ok=True)
 
-path = kagglehub.dataset_download("oddrationale/mnist-in-csv")
+path = kagglehub.dataset_download("debashis74017/stock-market-data-nifty-50-stocks-1-min-data")
 
 shutil.copytree(path, raw_data_dir, dirs_exist_ok=True)
